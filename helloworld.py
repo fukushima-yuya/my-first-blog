f7c48c2a23b72d6c5d765cc2d4e20c74d90bd424
fukushima-yuya <yuya.fukushima@beex-inc.com>
2019-09-06 17:10:47 +0900

    My Django Girls app, first commit

---
+++ helloworld.py
@@ -0,0 +1,12 @@
+print('Hello!World!')
+x =3
+y =5
+datetime = 23
+for i in range(1,datetime):
+    if i %x ==0:
+        print(i ** x)
+        
+    if i %y ==0:
+        print(i ** y)
+        
+
